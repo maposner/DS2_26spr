4a385c41f370918d7f1c745c0e18724cf34739d4
Prof. Michael Posner <michael.posner@villanova.edu>
2026-04-14 08:20:28 -0400

    test change

--- old-faithful-on-github/app.R
+++ old-faithful-on-github/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data - testing change"),
+    titlePanel("Old Faithful Geyser Data - test change"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
